cc2df9bcbb3e073ebb8b52f2c8277078cbccae87
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-14 15:22:20 -0500

    Variables

---
+++ HolaMundoJava/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaramos una variable
+        int speed;
+        // Asignamos un valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Rodo";
+        System.out.println(employeeName);
+    }
+}
