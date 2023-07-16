e406f4a6a1e6eceaed9759b2bb817aa256a23eb8
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-16 09:24:14 -0500

    creating static element

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,14 +1,21 @@
 public class Doctor {
-    int id;
+    // Atributos
+    static int id; // Autoincrement
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
     Doctor(String name){
         System.out.println("El nombre del doctor asignado es: " + name);
-
     }
     // Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,18 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Rodo");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Rodolfo Peña";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+        // System.out.println(Doctor.id);
+
+        showMenu();
     }
+
+
 }
