2df88ac2a82d5a8442e67724ebcae9d4024f7a8e
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-16 08:46:45 -0500

    Constructor method

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -3,6 +3,10 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(String name){
+        System.out.println("El nombre del doctor asignado es: " + name);
+
+    }
     // Comportamientos
     public void showName(){
         System.out.println(name);
