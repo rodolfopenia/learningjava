d21e9bc075129f485c7204560b8d966ae8d775dc
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-23 01:41:56 -0500

    Inheritance lesson

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,34 +1,14 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
+public class Doctor extends User {
     // Atributos
-    static int id; // Autoincrement
-    private String email;
-    private String name;
-
-    public static int getId() {
-        return id;
-    }
-
-    public static void setId(int id) {
-        Doctor.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    private String speciality;
+    
+    Doctor(String name, String email){
+        super(name, email);
+        System.out.println("El nombre del doctor asignado es: " + name);
+        this.speciality = speciality;
     }
 
     public String getSpeciality() {
@@ -38,28 +18,7 @@ public class Doctor {
     public void setSpeciality(String speciality) {
         this.speciality = speciality;
     }
-
-    private String speciality;
-
-
-    Doctor(){
-        System.out.println("Construyendo el Objeto Doctor");
-
-    }
-    Doctor(String name, String speciality){
-        System.out.println("El nombre del doctor asignado es: " + name);
-        id++;
-        this.name = name;
-        this.speciality = speciality;
-    }
     // Comportamientos
-    public void showName(){
-        System.out.println(name);
-    }
-    public void showId(){
-        System.out.println("ID Doctor: " + id);
-    }
-
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
     public void addAvailableAppointment(Date date, String time){
         availableAppointments.add(new AvailableAppointment(date, time));
