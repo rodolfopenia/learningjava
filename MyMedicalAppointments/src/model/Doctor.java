c509c70900d9946a8c5bb2695296de335939bb19
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-08-08 01:38:57 -0500

    Finishing the project

--- MyMedicalAppointments/src/model/Doctor.java
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -22,7 +24,8 @@ public class Doctor extends User {
     }
     // Comportamientos
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
-    public void addAvailableAppointment(Date date, String time){
+    public void addAvailableAppointment(String date, String time){
+
         availableAppointments.add(new AvailableAppointment(date, time));
     }
 
@@ -45,9 +48,14 @@ public class Doctor extends User {
         private int id;
         private Date date;
         private String time;
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
-        public AvailableAppointment(Date date, String time){
-            this.date = date;
+        public AvailableAppointment(String date, String time) {
+            try{
+                this.date = format.parse(date);
+            } catch(ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -62,6 +70,10 @@ public class Doctor extends User {
         public Date getDate() {
             return date;
         }
+        public String getDate(String date){
+            return format.format(date);
+        }
+
 
         public void setDate(Date date) {
             this.date = date;
